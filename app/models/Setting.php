b97b9c11c713901500ad9b90c0de000f2e5f66ce
Alberto Leal <albertoleal87@hotmail.com>
2015-09-30 17:23:56 -0500

    Se agrega funcionalidad para tomar configuraciones desde base de datos

--- app/models/Setting.php
+++ app/models/Setting.php
@@ -1,11 +1,38 @@
 <?php
 
 class Setting extends Eloquent {
+
+	protected $table = 'settings';
+
 	protected $guarded = array();
 
 	public static $rules = array(
 		'key' => 'required',
 		'value' => 'required',
-		'description' => 'required'
+		'description' => 'required',
 	);
+
+	public static $booleans = array(
+		'app.debug',
+		'database.log_query',
+		'app.log_in_console',
+	);
+
+	public static function setSettingsFromDB(){
+
+		$settings = Setting::all();
+
+		foreach($settings as $setting) {
+			if(in_array($setting->key, Setting::$booleans)){
+				$setting->value = Setting::parseBool($setting->value);
+			}
+			Config::set($setting->key,$setting->value);
+		}
+
+	}
+
+	public static function parseBool($value){
+		return (strtolower($value) == 'true') ? true : false;
+	}
+
 }
