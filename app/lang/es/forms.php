474f99506428a2594fafd9aca57881d907dddf06
Alberto Leal <albertoleal87@hotmail.com>
2015-10-09 23:15:24 -0500

    Se cargan archivos de lenguaje para español

---
+++ app/lang/es/forms.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+	'confirm_delete_title' => 'Confirmar eliminar',
+	'confirm_delete_message' => '¿Estás seguro que deseas eliminar este registro?',
+	'confirm_delete_btn_cancel' => 'Cancelar',
+	'confirm_delete_btn_delete' => 'Eliminar',
+
+);
---
+++ app/lang/es/modules/users.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	'title' => 'Usuarios',
+	'new_user' => 'Nuevo usuario',
+	'show_user' => 'Ver usuario',
+	'edit_user' => 'Editar usuario',
+	'delete_user' => 'Eliminar usuario',
+	'field_enable' => 'Activo',
+	'field_profile_id' => 'Perfil',
+	'field_name' => 'Nombre',
+	'field_last_name' => 'Apellido paterno',
+	'field_mother_last_name' => 'Apellido materno',
+	'field_email' => 'E-mail',
+	'field_options' => 'Opciones',
+	'there_are_no_users' => 'No hay usuarios registrados.',
+	
+);
